011b3445916e659b640c964958f4f753723d991c
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-11-07 13:33:48 -0500

    chore(embark-solc): add eslintrc and fix linting errors

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const Compiler = require("./lib/Compiler");
 
 module.exports = (embark) => {
-	embark.registerCompiler('.sol', compileSolc);
-	function compileSolc(contractFiles, options, cb) {
-		if(!contractFiles || !contractFiles.length) {
-			return cb();
-		}
-		Compiler.compileSolc(embark, contractFiles, cb);
-	}
+  embark.registerCompiler('.sol', (contractFiles, options, cb) => {
+    if (!contractFiles || !contractFiles.length) {
+      return cb();
+    }
+    Compiler.compileSolc(embark, contractFiles, cb);
+  });
 };
