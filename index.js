1e02abf8647d2bee6a5a16b53f42f894c571682c
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-11-07 14:47:36 -0500

    fix(embark-solc): stop embark when solc version too low
    Fixes #8

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 const Compiler = require("./lib/Compiler");
 
 module.exports = (embark) => {
+  if (embark.config.embarkConfig.versions.solc) {
+    // Check solc version
+    Compiler.getSolcVersion(embark.logger, (err, version) => {
+      if (err) {
+        embark.logger.error(err);
+        process.exit(1);
+      }
+      const wantedVer = embark.config.embarkConfig.versions.solc.split('.').map(ver => parseInt(ver, 10));
+      const currentVer = version.split('.').map(ver => parseInt(ver, 10));
+      if (wantedVer[0] > currentVer[0] || wantedVer[1] > currentVer[1] || wantedVer[2] > currentVer[2]) {
+        embark.logger.error(`Current version of solc lower than version in embark.json`);
+        embark.logger.error(`Current: ${version} | Wanted: ${embark.config.embarkConfig.versions.solc}`);
+        process.exit(1);
+      }
+    });
+  }
+
+
   embark.registerCompiler('.sol', (contractFiles, options, cb) => {
     if (!contractFiles || !contractFiles.length) {
       return cb();
