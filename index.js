0fdea9d20c92e5cbfc546cea8e473a7e652050cf
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-01-16 11:31:57 -0500

    fix: fix race condition by using the new compiler api

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 /*global require, module*/
 const Compiler = require("./lib/Compiler");
+const semver = require('semver');
 
 module.exports = (embark) => {
   if (embark.config.embarkConfig.versions.solc) {
-    // Check solc version
-    Compiler.getSolcVersion(embark.logger, (err, version) => {
-      if (err) {
-        embark.logger.error(err);
-        embark.logger.error("Error getting solc's version. Will default back to Embark's commpiler");
-        return;
-      }
-      const wantedVer = embark.config.embarkConfig.versions.solc.split('.').map(ver => parseInt(ver, 10));
-      const currentVer = version.split('.').map(ver => parseInt(ver, 10));
-      if (wantedVer[0] > currentVer[0] || wantedVer[1] > currentVer[1] || wantedVer[2] > currentVer[2]) {
-        embark.logger.warn(`Current version of solc lower than version in embark.json`);
-        embark.logger.warn(`Current: ${version} | Wanted: ${embark.config.embarkConfig.versions.solc}`);
-        embark.logger.warn('Will default back to Embark\'s compiler');
-        return;
-      }
-
-      embark.registerCompiler('.sol', (contractFiles, options, cb) => {
-        if (!contractFiles || !contractFiles.length) {
-          return cb();
+    const versionPromise = new Promise(function(resolve, reject) {
+      // Check solc version
+      Compiler.getSolcVersion(embark.logger, (err, version) => {
+        if (err) {
+          embark.logger.error(err);
+          embark.logger.error("Error getting solc's version. Will default back to Embark's compiler");
+          return reject(err);
         }
-        Compiler.compileSolc(embark, contractFiles, embark.config.contractDirectories, cb);
+        if (semver.lt(version, embark.config.embarkConfig.versions.solc)) {
+          embark.logger.warn(`Current version of solc lower than version in embark.json`);
+          embark.logger.warn(`Current: ${version} | Wanted: ${embark.config.embarkConfig.versions.solc}`);
+          embark.logger.warn('Will default back to Embark\'s compiler');
+          return reject(new Error('Bad version'));
+        }
+        resolve();
       });
     });
+
+    embark.registerCompiler('.sol', (contractFiles, options, cb) => {
+      if (!contractFiles || !contractFiles.length) {
+        return cb();
+      }
+      versionPromise.then(() => {
+        Compiler.compileSolc(embark, contractFiles, embark.config.contractDirectories, cb);
+      }).catch(_e => {
+        // Need to default to Embark's compiler
+        cb(null, false);
+      });
+    });
+
   }
 };
