2c96050b90ac76a89edae1925d41037798023714
emizzle <ericmastro@status.im>
2019-01-31 13:37:20 +1100

    fix(@embark/embark-solc): Support recursively remapping imports
    Changes include:
    - Before compilation, for each contract file being compiled, recursively remaps the contract's imports.
    - Allowed directories for compilation updated based on the remapped import paths for the contract file.
    - Support for code coverage flag.
    - Fix for unhandled promise rejection when attempting to use an insufficient version of solc.

--- index.js
+++ index.js
@@ -6,31 +6,26 @@ module.exports = (embark) => {
   if (embark.config.embarkConfig.versions.solc) {
     const versionPromise = new Promise(function(resolve, reject) {
       // Check solc version
-      Compiler.getSolcVersion(embark.logger, (err, version) => {
-        if (err) {
-          embark.logger.error(err);
-          embark.logger.error("Error getting solc's version. Will default back to Embark's compiler");
-          return reject(err);
-        }
-        if (semver.lt(version, embark.config.embarkConfig.versions.solc)) {
-          embark.logger.warn(`Current version of solc lower than version in embark.json`);
-          embark.logger.warn(`Current: ${version} | Wanted: ${embark.config.embarkConfig.versions.solc}`);
-          embark.logger.warn('Will default back to Embark\'s compiler');
-          return reject(new Error('Bad version'));
-        }
-        resolve();
-      });
+      
     });
 
     embark.registerCompiler('.sol', (contractFiles, options, cb) => {
       if (!contractFiles || !contractFiles.length) {
         return cb();
       }
-      versionPromise.then(() => {
-        Compiler.compileSolc(embark, contractFiles, embark.config.contractDirectories, cb);
-      }).catch(_e => {
-        // Need to default to Embark's compiler
-        cb(null, false);
+      Compiler.getSolcVersion(embark.logger, (err, version) => {
+        if (err) {
+          embark.logger.error(err);
+          embark.logger.error("Error getting solc's version. Will default back to Embark's compiler");
+          return cb(null, false);
+        }
+        if (semver.lt(version, embark.config.embarkConfig.versions.solc)) {
+          embark.logger.warn(`Current version of solc lower than version in embark.json`);
+          embark.logger.warn(`Current: ${version} | Wanted: ${embark.config.embarkConfig.versions.solc}`);
+          embark.logger.warn('Will default back to Embark\'s compiler');
+          return cb(null, false);
+        }
+        Compiler.compileSolc(embark, contractFiles, embark.config.contractDirectories, options, cb);
       });
     });
 
